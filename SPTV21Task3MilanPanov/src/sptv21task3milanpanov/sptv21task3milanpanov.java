aec05af9f6a75f008b51a253700e2f95248c8294
MilanPanov533 <113300988+MilanPanov533@users.noreply.github.com>
2022-09-27 11:09:32 +0300

    Add files via upload

---
+++ SPTV21Task3MilanPanov/src/sptv21task3milanpanov/sptv21task3milanpanov.java
@@ -0,0 +1,34 @@
+package sptv21task3milanpanov;
+
+import java.util.Scanner;
+
+public class sptv21task3milanpanov{
+    public static int year2;
+    static enum month {Января,Февраля, Марта, Апреля, Мая, ИЮНЯ, Июня, Августа, Сентября, Октября, Ноября, Декабря}
+    public static void main(String[] args) {
+        System.out.println("Имя(Латиницей) -  ");
+        Scanner scanner_name = new Scanner(System.in);
+        String Name = scanner_name.nextLine();
+        System.out.println("Фамили(Латиницей) -  ");
+        Scanner scanner_lastname = new Scanner(System.in);
+        String Lastname = scanner_lastname.nextLine();
+        System.out.println("Ваш ID код - ");
+        Scanner scanner_isikukood = new Scanner(System.in);
+        String Isikukood = scanner_isikukood.nextLine();
+        String year = String.valueOf(Isikukood);
+        year =year.substring(1, 3);
+        int year1 = new Integer(year);
+        if(year1>0 && year1<24){
+            year2 = 20;
+        } else {
+            year2 = 19;
+        }
+        String mounth1 = String.valueOf(Isikukood);
+        mounth1 = mounth1.substring(3,5);
+        int mounth2 = new Integer(mounth1);
+        String day = String.valueOf(Isikukood);
+        day = day.substring(5,7);
+
+        System.out.println(Name + " " + Lastname + " родился " +year2+year+" года "+ day + " " + month.values()[mounth2-1]);
+    }
+}
